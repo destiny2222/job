eabfa9b1f2936f902f40bd480bc882e9687e29b2
destiny2222 <oghenekumeerieme@gmail.com>
2022-08-10 19:07:05 +0100

    first commit

--- gen/models.py
+++ gen/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+from django.utils.timezone import now
 # Create your models here.
 
 
@@ -15,13 +15,13 @@ class Generate(models.Model):
     payment_method = models.CharField(max_length=225, choices=choice_nethod)
     amount = models.DecimalField(max_digits=225, decimal_places=2)
     hashtag = models.CharField(max_length=225)
-    sent_date = models.TimeField(default=timezone.now, null=True, blank=True)
+    sent_date = models.TimeField(auto_now_add=now, null=True, blank=True)
 
     def __str__(self):
         return self.fullname
 
 class PaymentSlip(models.Model):
     name = models.CharField(max_length=225)
-    number = models.CharField(max_length=225)
+    # number = models.CharField(max_length=225)
     price = models.DecimalField(max_digits=225, decimal_places=2)
     # sent_date = models.TimeField(default=timezone.now, null=True, blank=True)
